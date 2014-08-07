4ecd1d1c7a33024fc0a18360e643729626dabf9c
Eivind Mikkelsen <eivindm@conduct.no>
2014-08-07 15:36:30 +0200

    Fixing update_admin_crypto.sh tool

--- files/update_admin_crypto.sh
+++ files/update_admin_crypto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ENC_PWD=``./cli.sh encrypt $2 | grep BEGIN -A11 | grep -v ENCRYPTED | tr '\n' ' '`
+ENC_PWD=`./cli.sh encrypt $1 2>/dev/null | grep BEGIN -A11 | grep -v ENCRYPTED | tr '\n' ' '`
 echo "UPDATE internaluser SET pwd = '${ENC_PWD}' WHERE objectid = 'openidm-admin';"
